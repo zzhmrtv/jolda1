72a03b59a4188cfa750721c855534ade2443c456
Tamerlan <98075663+zzhmrtv@users.noreply.github.com>
2025-12-16 15:32:41 -0500

    Delete app directory

--- app/lib/theme.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const blue = Color(0xFF3B82F6);
-const textDark = Color(0xFF0F172A);
-const textMuted = Color(0xFF475569);
-const border = Color(0xFFE2E8F0);
